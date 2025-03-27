29b477c1df32f844efe7cd01ec0ddc95f895e20f
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-03-27 18:31:08 +0000

    add pull_id_task method to TaskQueue

--- ngtask_queue/src/lib.rs
+++ ngtask_queue/src/lib.rs
@@ -31,7 +31,7 @@ impl TaskQueue {
         Arc::new(Mutex::new(TaskQueue { is_initialised, id_queue, category_queues }))
     }
 
-    pub fn push_id_task_to_queue(&mut self, task: IdTask) -> usize {
+    pub fn push_id_task(&mut self, task: IdTask) -> usize {
         if task.id == String::new() || task.payload == String::new() || self.id_queue.contains_key(&task.id) {
             0
         } else {
@@ -42,13 +42,13 @@ impl TaskQueue {
         }
     }
 
-    pub fn push_category_task_to_queue(&mut self, task: CategoryTask) -> (usize, usize) {
+    pub fn push_category_task(&mut self, task: CategoryTask) -> (usize, usize) {
         if task.category == String::new() || task.payload == String::new() {
             return (0,0)
         } 
         match self.category_queues.get_mut(&task.category) {
             Some(queue) => {
-                let queue_size = push_category_task(queue, task);
+                let queue_size = push_category_task_to_existing_queue(queue, task);
                 (self.category_queues.len(), queue_size)
             },
             None => {
@@ -59,10 +59,14 @@ impl TaskQueue {
             }
         }
     }
+
+    pub fn pull_id_task(&mut self, id: String) -> Option<String> {
+        self.id_queue.remove(&id)
+    }
 }
 
 
-fn push_category_task(queue: &mut Mutex<Vec<String>>, task: CategoryTask) -> usize {
+fn push_category_task_to_existing_queue(queue: &mut Mutex<Vec<String>>, task: CategoryTask) -> usize {
     match queue.lock() {
         Ok(mut opened_queue) => {
             opened_queue.push(task.payload);
@@ -99,7 +103,7 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let i = task_queue.push_id_task_to_queue(id_task);
+                let i = task_queue.push_id_task(id_task);
                 assert_eq!(i, 1);
                 assert_eq!(task_queue.id_queue.len(), 1)
             },
@@ -120,7 +124,7 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let i = task_queue.push_id_task_to_queue(id_task1);
+                let i = task_queue.push_id_task(id_task1);
                 assert_eq!(i, 1);
                 assert_eq!(task_queue.id_queue.len(), 1)
             },
@@ -136,7 +140,7 @@ mod tests {
         };
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let i = task_queue.push_id_task_to_queue(id_task2);
+                let i = task_queue.push_id_task(id_task2);
                 assert_eq!(i, 0);
                 assert_eq!(task_queue.id_queue.len(), 1)
             },
@@ -157,7 +161,7 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let i = task_queue.push_id_task_to_queue(id_task);
+                let i = task_queue.push_id_task(id_task);
                 assert_eq!(i, 0);
                 assert_eq!(task_queue.id_queue.len(), 0)
             },
@@ -178,7 +182,7 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let (number_of_queue, queue_len) = task_queue.push_category_task_to_queue(task);
+                let (number_of_queue, queue_len) = task_queue.push_category_task(task);
                 assert_eq!(number_of_queue, 1);
                 assert_eq!(queue_len, 1);
             },
@@ -199,7 +203,7 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let (number_of_queue, queue_len) = task_queue.push_category_task_to_queue(task);
+                let (number_of_queue, queue_len) = task_queue.push_category_task(task);
                 assert_eq!(number_of_queue, 1);
                 assert_eq!(queue_len, 1);
             },
@@ -216,7 +220,7 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let (number_of_queue, queue_len) = task_queue.push_category_task_to_queue(task);
+                let (number_of_queue, queue_len) = task_queue.push_category_task(task);
                 assert_eq!(number_of_queue, 1);
                 assert_eq!(queue_len, 2);
             },
@@ -237,7 +241,7 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let (number_of_queue, queue_len) = task_queue.push_category_task_to_queue(task);
+                let (number_of_queue, queue_len) = task_queue.push_category_task(task);
                 assert_eq!(number_of_queue, 0);
                 assert_eq!(queue_len, 0);
             },
@@ -258,12 +262,71 @@ mod tests {
 
         match task_queue_arc.lock() {
             Ok(mut task_queue) => {
-                let (number_of_queue, queue_len) = task_queue.push_category_task_to_queue(task);
+                let (number_of_queue, queue_len) = task_queue.push_category_task(task);
                 assert_eq!(number_of_queue, 0);
                 assert_eq!(queue_len, 0);
             },
             Err(error) => {
                 println!("Failed to open queue: {:?}", error);
+                assert!(false)
+            }
+        };
+    }
+
+    #[test]
+    fn id_not_in_queue_test_pull_id_task_from_queue() {
+        let task_queue_arc = TaskQueue::initialise();
+        
+        match task_queue_arc.lock() {
+            Ok(mut task_queue) => {
+                let queue_response = task_queue.pull_id_task(String::from("test"));
+                assert_eq!(queue_response, None)
+            },
+            Err(error) => {
+                println!("Failed to open queue {:?}", error);
+                assert!(false)
+            }
+        };
+    }
+
+    #[test]
+    fn id_not_valid_test_pull_id_task_from_queue() {
+        let task_queue_arc = TaskQueue::initialise();
+        
+        match task_queue_arc.lock() {
+            Ok(mut task_queue) => {
+                let queue_response = task_queue.pull_id_task(String::new());
+                assert_eq!(queue_response, None)
+            },
+            Err(error) => {
+                println!("Failed to open queue {:?}", error);
+                assert!(false)
+            }
+        };
+    }
+
+    #[test]
+    fn id_valid_and_exist_test_pull_id_task_from_queue() {
+        let task_queue_arc = TaskQueue::initialise();
+        let payload = String::from("Do something"); 
+        let task = IdTask {
+            id: String::from("test"),
+            payload: payload.clone()
+        };
+
+        match task_queue_arc.lock() {
+            Ok(mut task_queue) => {
+                task_queue.push_id_task(task);
+                match task_queue.pull_id_task(String::from("test")) {
+                    Some(payloud) => {
+                        assert_eq!(payloud, payload);
+                        assert_eq!(task_queue.id_queue.len(), 0)
+                    },
+                    None => assert!(false)
+                }
+            },
+            Err(error) => {
+                println!("Failed to open queue {:?}", error);
                 assert!(false)
             }
         };
