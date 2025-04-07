88c57e762ade20cbaebb977c69cd51c6abf4a749
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-04-07 20:49:59 +0100

    call receiver from main and pass in a local path

--- ngtq_bus_service/src/main.rs
+++ ngtq_bus_service/src/main.rs
@@ -1,5 +1,15 @@
 mod receiver;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    println!("Starting Bus Service...");
+    let receiver_instance = receiver::Receiver {
+
+    };
+
+    match receiver_instance.start_receiver( "/tmp/resu_ipc_socket", &true) {
+        Ok(_) => println!("Is this reachable?"),
+        Err(error) => println!("Recevier failed: {}", error)
+    }
+
+    Ok(())
 }
