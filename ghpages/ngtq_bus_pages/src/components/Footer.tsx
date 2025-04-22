d033d5fa59479a9f6acddfdd867521e9aaa56181
NGDeveloper125 <NGDeveloper125@outlook.com>
2025-04-22 19:05:58 +0100

    Add footer components & contributors section in footer

---
+++ ghpages/ngtq_bus_pages/src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../style/Footer.css";
+
+function Footer() {
+    return (
+    <div className="footer">
+        <div className="contributors">
+            <h3>Contributors</h3>
+            <ul>
+                <li>NGDeveloper : NGDeveloper125@Outlook.com</li>
+            </ul>
+        </div>
+    </div>
+    );
+}
+
+export default Footer;
